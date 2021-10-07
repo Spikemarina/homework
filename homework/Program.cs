9396a6032c375322f9b2e3f6172194b84f55e32a
Marina <Marina@DESKTOP-HOMTD73>
2021-10-07 20:52:20 +0300

    Add project files.

---
+++ homework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Marina!");
+            Console.WriteLine();
+            Console.Write("I'm working at the airport security.\nI'm married,have a daughter, dog and a cat.");
+            Console.WriteLine();
+            Console.WriteLine("I love reading books, travel and playing videogames.\nBut most of all I love to  spend time with  my daughter.");
+            Console.WriteLine("And I'm looking forward to learn something new this year!");
+            Console.ReadLine();
+            Console.WriteLine("Hello, my name is Marina!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
